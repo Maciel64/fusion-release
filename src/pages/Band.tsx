4cfe53fa6c3b17ba6e6c1dc4fab3fc85092bdc40
Maciel Gomes Suassuna Júnior <macielsuassuna14@gmail.com>
2023-10-17 18:34:49 -0300

    [FIX]: Changed videos again.

--- src/pages/Band.tsx
+++ src/pages/Band.tsx
@@ -3,9 +3,9 @@ import { Button } from "../components/Form"
 import Title from "../components/Title"
 
 import greetings from "../assets/video/boasvindas.mp4"
-import professionalVideo from "../assets/video/profissional.mp4"
-import partnerVideo from "../assets/video/donodeclinica_erro.mp4"
-import ownerVideo from "../assets/video/donodeclinica.mp4"
+import professionalVideo from "../assets/video/amarela_.mp4"
+import partnerVideo from "../assets/video/roxa_.mp4"
+import ownerVideo from "../assets/video/branca_.mp4"
 
 
 import VideoCard from "../components/VideoCard"
@@ -21,25 +21,25 @@ export default ({ setUIPhase, setGradient }: any) => {
         whoAreYou: {
             text: "Escolha a opção que mais se encaixa para você.",
             video: greetings,
-            gradient: "gradient"
+            gradient: "gradient",
         },
         professional: {
-            text: "Profissional da saúde",
+            text: "Pulseira Amarela",
             video: professionalVideo,
             color: "#f0e211",
             gradient: "gradient--to-yellow"
         },
         owner: {
-            text: "Dono de clínica",
+            text: "Pulseira Roxa",
             video: partnerVideo,
             color: "#f3f3f3",
-            gradient: "gradient--to-white"
+            gradient: "gradient--to-pink"
         },
         partner: {
-            text: "Parceiro",
+            text: "Pulseira Branca",
             video: ownerVideo,
             color: "#911d80",
-            gradient: "gradient--to-pink"
+            gradient: "gradient--to-white"
         },
     }
 
@@ -75,9 +75,9 @@ export default ({ setUIPhase, setGradient }: any) => {
             
             {type === "whoAreYou" && (
                 <div className="flex gap-5 flex-col p-5 opacity-0 transition-opacity" ref={buttonsContainerRed}>
-                    <Button text="Profissional da saúde" onClick={() => onButtonClicked("professional")} />
-                    <Button text="Dono de clínica" onClick={() => onButtonClicked("owner")} />
-                    <Button text="Parceiro" onClick={() => onButtonClicked("partner")} />
+                    <Button text="Profissional da saúde" onClick={() => onButtonClicked("professional")} style={{ border: "1px solid #f1e60e" }} />
+                    <Button text="Dono de clínica" onClick={() => onButtonClicked("owner")} style={{ border: "1px solid #742165" }} />
+                    <Button text="Parceiro" onClick={() => onButtonClicked("partner")} style={{ border: "1px solid #ffffff" }} />
                 </div>
             )}
         </>
