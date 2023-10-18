c46d5bfed20d16b6cc110bc259acab6a0b555a7b
Maciel Gomes Suassuna Júnior <macielsuassuna14@gmail.com>
2023-10-18 17:11:47 -0300

    [FIX]: Chaged to use only mobile banner.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,20 @@
-import { useState } from "react";
 import { Button } from "../components/Form";
 
-import bannerDesktop from "../assets/img/banner.png";
 import bannerMobile from "../assets/img/bannerMobile.jpeg";
 
 
 export default ({ setUIPhase }: any) => {
-  const [width] = useState(window.innerWidth)
   const onButtonClicked = () => {
     setUIPhase("band");
   };
 
   return (
     <>
-      {width > 512 ? (
-        <img
-          src={bannerDesktop}
-          alt="Banner Que saúde o futuro nos reserva?"
-          className="h-[50vh] justify-self-start"
-        />
-      ) : (
         <img
           src={bannerMobile}
           alt="Banner Que saúde o futuro nos reserva?"
           className="h-[80vh] justify-self-start"
         />
-      )}
 
       <div className="flex w-[300px]">
         <Button
